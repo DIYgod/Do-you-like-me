3b733c3857d4a6cf66ca9f7fe42b2c771250bc7d
范文杰 <diy.d.god@gmail.com>
2015-10-02 23:41:59 +0800

    prevent multiple ajax

--- js/like.js
+++ js/like.js
@@ -2,13 +2,15 @@ $.getJSON("./like.php?action=get", function (data) {
     $('.like-vote span').html(data.like);
 });
 $('.like-vote').click(function () {
-    $.getJSON("./like.php?action=add", function (data) {
-        if (data.success) {
-            $('.like-vote span').html(data.like);
-            $('.like-title').html('我也喜欢你 (*≧▽≦)');
-        }
-        else {
-            $('.like-title').html('你的爱我已经感受到了~');
-        }
-    });
+    if ($('.like-title').html() === 'Do you like me?') {
+        $.getJSON("./like.php?action=add", function (data) {
+            if (data.success) {
+                $('.like-vote span').html(data.like);
+                $('.like-title').html('我也喜欢你 (*≧▽≦)');
+            }
+            else {
+                $('.like-title').html('你的爱我已经感受到了~');
+            }
+        });
+    }
 });
